d7dba407b15443c11aa9b550e04eeb6b1f87d2b2
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-29 20:48:40 -0300

    initial files

---
+++ 01Basics/01types.py
@@ -0,0 +1,19 @@
+"""
+
+Types in Python
+
+str - string - texts like 'this' or "this"
+int - integer - 123456 0 -10 -20 -30 15000
+float - real number - 10.50 1.5 -10.10
+bool - logical -  True/False a==a a!=not(a)
+
+"""
+
+print('Kant', type('Kant'));
+print(10, type(10));
+print(25.2, type(25.2));
+print(10!=100, type(10!=100));
+
+print('Kant', type(bool('Kant')));
+print('', type(bool(''))); ##null value
+
